70c7b9e00439d0271f62e99fad7185915296909f
bigdata1 <tsbslteisrhy@naver.com>
2020-07-27 16:49:08 +0900

    2020/07/23 수정

--- Ch02/2-4.py
+++ Ch02/2-4.py
@@ -3,7 +3,6 @@
     이름 : 유효진
     내용 : 파이썬 Hadoop 실습하기
 """
-
 from pywebhdfs.webhdfs import PyWebHdfsClient as hadoop
 
 #Hadoop 접속
@@ -11,21 +10,7 @@ hdfs = hadoop(host='192.168.100.101', port='50070', user_name='root')
 
 #Local /home/bigdata/naver/naver-20-xx-xx 를 하둡 /naver/ 복사
 naver = 'home/bigdata/naver/'
-hdfs.get_file_dir_status(naver)
-{
-    "FileStatus":{
-        "accessTime":0,
-        "blockSize":0,
-        "group":"hdfs",
-        "length":0,
-        "modificationTime":1371737704208,
-        "owner":"root",
-        "pathSuffix":"",
-        "permission":"755",
-        "replication":0,
-        "type":"DIRECTORY"
-    }
-}
+hdfs.append_file('/home/bigdata/naver/', 'naver-20-07-15')
 
 #Local /home/bigdata/naver/naver-20-xx-xx 를 삭제
 
--- Ch02/movie.py
+++ Ch02/movie.py
@@ -9,7 +9,22 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from datetime import datetime
 from pymongo import MongoClient as mongo
-import time
+import time, logging
+
+#로거 생성
+logger = logging.getLogger('movie_logger')
+logger.setLevel(logging.INFO)
+
+#로그 포맷 설정
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+#핸들러 생성
+fileHandler = logging.FileHandler('./movie.log')
+fileHandler.setLevel(logging.INFO)
+fileHandler.setFormatter(formatter)
+
+#로거 + 핸들러 연결
+logger.addHandler(fileHandler)
 
 #크롬 가상브라우저 실행
 chrome_options = webdriver.ChromeOptions()
@@ -19,35 +34,43 @@ chrome_options = webdriver.ChromeOptions()
 #browser = webdriver.Chrome('../Ch01/chromedriver.exe', chrome_options=chrome_options)
 browser = webdriver.Chrome('../Ch01/chromedriver.exe')
 browser.implicitly_wait(3)
+logger.info('크롬 가상브라우저 실행 완료')
 
 # 랭크 페이지 번호 변수 선언
 page = 1
 
 # 1차:랭크 페이지
 while True:
+    logger.info('첫번째 while 시작')
 
     # 랭크 변수 선언(0 ~ 49)
     j = 0
-    #time.sleep(1)
+    time.sleep(1)
 
     while True:
+        logger.info('두번째 while 시작')
+
         #네이버 영화 랭킹 평점순 이동
         browser.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200720&page='+str(page))
         browser.implicitly_wait(3)
+        logger.info('네이버 영화 랭킹 평점순 이동 page=%d'%page)
 
         try:
             #영화 랭킹 클릭
             ranks = browser.find_elements_by_css_selector('#old_content > table > tbody > tr > td.title > div > a')
             ranks[j].click()
             browser.implicitly_wait(3)
+            logger.info('영화 랭킹 클릭')
         except:
+            logger.warning('두번째 while try 예외발생')
             break
 
         #영화 평점 탭 클릭
-        #review_tab = browser.find_element_by_css_selector('#movieEndTabMenu > li:nth-child(5) > a')
-        review_tab = browser.find_element_by_class_name('tab05').click()
-        #review_tab.click()
+        review_tab = browser.find_element_by_css_selector('#movieEndTabMenu > li > a.tab05')
+        #review_tab = browser.find_element_by_class_name('tab05').click()
+        review_tab.click()
         browser.implicitly_wait(3)
+        logger.info('영화 평점 탭 클릭')
 
         #영화제목 수집
         tit_tag = browser.find_element_by_css_selector('#content > div.article > div.mv_info_area > div.mv_info > h3 > a')
@@ -56,21 +79,25 @@ while True:
 
         code = href[i:]
         title = tit_tag.text
+        logger.info('영화 제목 : %s' % title)
+
 
         # 평점, 리뷰, 날짜 수집을 위해 현재 브라우저를 iframe 페이지로 전환
         browser.switch_to.frame('pointAfterListIframe')
         browser.implicitly_wait(3)
+        logger.info('iframe 전환')
 
         # 최신순 클릭
         tag_latest_a = browser.find_element_by_css_selector('#orderCheckbox > ul.sorting_list > li:nth-child(2) > a')
         tag_latest_a.click()
         browser.implicitly_wait(3)
+        logger.info('최신순 클릭')
 
         #MongoDB 접속, DB선택, Collection선택
         #conn = mongo('mongodb://rhj:1234@192.168.100.101:27017')
         conn = mongo('mongodb://rhj:1234@192.168.50.82:27017/rhj')
         db = conn.get_database('rhj')
-        collection = db.get_collection('movie_test3')
+        collection = db.get_collection('movie_0723_6')
 
         #현재 페이지 설정
         page_num = 1
@@ -83,7 +110,10 @@ while True:
                 page_btn = browser.find_element_by_id(page_id)
                 page_btn.click()
                 browser.implicitly_wait(3)
+                logger.info('현재 페이지 : %d' % page_num)
+
             except:
+                logger.warning('클릭할 페이지 없음')
                 break
 
             # 평점, 리뷰, 날짜 수집
@@ -104,7 +134,7 @@ while True:
             #Insert 실행
             collection.insert_many(list_data)
 
-            print(page_num, '페이지 insert 완료.')
+            print(title, page_num, '페이지 insert 완료.')
             page_num += 1
 
         # 그 다음 영화 리뷰 수집을 위한 랭크 변수 증가
